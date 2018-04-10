83353c7040a79b498d18c82f9438da34e7efb31e
Daniela Sainz <dasainz18@gmail.com>
2018-04-10 13:15:03 -0400

    Initial commit

--- src/com/Sainz/Main.java
+++ src/com/Sainz/Main.java
@@ -1,17 +1,5 @@
 package com.Sainz;
 
-/*
-Modify your existing application. Change the Integer to a Book and prompt the user to enter the title, author, description, price and inStock. The user should be able to enter as many books as they wish.
-Create a Book class that allows for Book title, author, description, price and isInStock variables
-The Book class shall contain private member variables, a default constructor, an overloaded constructor and a method called getDisplayText() which prints the author, title and description.
-The application shall also contain getters and setters for each private member variable.
-
-**Create another class called BookApp**
- which contains the main method. In the main method you shall create an instance of a Book and print the Author, Title and Description.
-Publish your solution to GitHub and submit your GitHub url as the assignment.
- */
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -21,7 +9,6 @@ public class Main {
 
         Scanner keyboard = new Scanner(System.in);
         boolean done = false;
-        // ? boolean inStock;
         String result = "";
         Book newBook;
 
@@ -63,7 +50,7 @@ public class Main {
             } else {
                 done = false;
             }
-            //don't understand purpose of while below... sorry, Dave.
+            //don't understand purpose of while below...
         } while (done);
 
         System.out.println("==================");
